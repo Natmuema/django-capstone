9cc98fe73dfd77c84ad3bba38e8e7965dca6f5c9
Kalondu Muema <30372680+Natmuema@users.noreply.github.com>
2024-08-12 19:56:42 +0300

    Update settings.py

--- zindua/settings.py
+++ zindua/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'zindua_alumni_db',
         'USER': 'postgres',
-        'PASSWORD': 'K@londu70',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
